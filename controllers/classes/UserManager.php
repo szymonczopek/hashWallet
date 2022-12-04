633d0685b430b991351f0fb3857a2d2b2d922982
szymonczopek <szymon.czopek@wp.pl>
2022-12-05 00:08:41 +0100

    tests

--- controllers/classes/UserManager.php
+++ controllers/classes/UserManager.php
@@ -7,50 +7,42 @@ class UserManager {
  function login($db) {
  //funkcja sprawdza poprawność logowania
  //wynik - id użytkownika zalogowanego lub -1
- $validate = array(
-                'login' => ['fil'=> FILTER_SANITIZE_FULL_SPECIAL_CHARS, 'filter'  => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '@^[a-ząęłńśćźżóA-Z0-9\!\@\#\$\%\^\&\*]{1,25}$@'] ],
-                'passwd'  => ['fil'=> FILTER_SANITIZE_FULL_SPECIAL_CHARS, 'filter'  => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '@^[a-ząęłńśćźżóA-Z0-9\!\@\#\$\%\^\&\*]{1,25}$@'] ]
- );
+     $data=$this->validateLogin(INPUT_POST);
 
- $dane = filter_input_array(INPUT_POST, $validate);
-
- $login = $dane["login"];
- $passwd = $dane["passwd"];
+    $login = $data["login"];
+    $passwd = $data["passwd"];
  
- 
-$userId = $db->selectUser($login, $passwd, "users");
+    $userId = $db->selectUser($login, $passwd, "users");
 
-     
-     if ($userId >= 0) { //Poprawne dane
+        if ($userId >= 0)
+        { //Poprawne dane
 
-     session_start();
-     $_SESSION['login']=$login;
-     $_SESSION['passwd']=$passwd;
-     $_SESSION['userId']=$userId;
+             session_start();
+             $_SESSION['login'] = $login;
+             $_SESSION['passwd'] = $passwd;
+             $_SESSION['userId'] = $userId;
 
+
+
+         //usuń wszystkie wpisy historyczne dla użytkownika o $userId
+             $sql="DELETE FROM logged_in_users WHERE userId='$userId'";
+             $db->delete($sql);
+
+             $date = $this->getCurrentDate();
+             $id_sesji = session_id();
+
+             $sql="INSERT INTO logged_in_users VALUES ('$id_sesji','$userId','$date')";
+             $db->insert($sql);
+        }
     
-     
- //usuń wszystkie wpisy historyczne dla użytkownika o $userId
-     $sql="DELETE FROM logged_in_users WHERE userId='$userId'";
-     $db->delete($sql);
-
-     $data=new DateTime('now');
-     $data=$data->format("Y-m-d H:i:s");
-
-     $id_sesji=session_id();
-     
-     $sql="INSERT INTO logged_in_users VALUES ('$id_sesji','$userId','$data')";
-     $db->insert($sql); 
+    return $userId;
  }
-    
- return $userId;
-      }
  
     
     function logout($db) {
 
      session_start();
-     $id_sesji=session_id();
+     $idSession=session_id();
 
      if ( isset($_COOKIE[session_name()]) )
      {
@@ -58,9 +50,24 @@ $userId = $db->selectUser($login, $passwd, "users");
      }
    session_destroy();
      
-     $db->delete("DELETE FROM logged_in_users WHERE sessionId='$id_sesji' ");
+     $db->delete("DELETE FROM logged_in_users WHERE sessionId='$idSession' ");
         
     header("location: ../index.php");
  }
+    function validateLogin($input)
+    {
+        $validate = array(
+            'login' => ['fil' => FILTER_SANITIZE_FULL_SPECIAL_CHARS, 'filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '@^[a-ząęłńśćźżóA-Z0-9\!\@\#\$\%\^\&\*]{1,25}$@']],
+            'passwd' => ['fil' => FILTER_SANITIZE_FULL_SPECIAL_CHARS, 'filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '@^[a-ząęłńśćźżóA-Z0-9\!\@\#\$\%\^\&\*]{1,25}$@']]
+        );
+
+        $data = filter_input_array($input, $validate);
+        return $data;
+    }
+    function getCurrentDate(){
+        $date=new DateTime('now');
+        $date=$date->format("Y-m-d H:i:s");
+        return $date;
+    }
  
 }
---
+++ tests/UserTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+include 'controllers/classes/UserManager.php';
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    private $user;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->user = new UserManager();
+    }
+
+
+    public function testValidateLogin()
+    {
+        $input=[
+            'login'=>'szymon',
+            'passwd'=>'haslo',
+
+        ];
+
+
+        $result = $this->user->validateLogin($input);
+
+        $result = in_array(null, $result);
+
+// Assert that $result is true
+        $this->assertTrue($result);
+
+    }
+
+
+
+
+
+
+
+}
