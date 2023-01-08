080493381d843af6d0af9431a153f3f97f55e8f6
szymonczopek <szymon.czopek@wp.pl>
2023-01-08 21:58:00 +0100

    block

--- bsibase.sql
+++ bsibase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 08 Sty 2023, 20:00
+-- Czas generowania: 08 Sty 2023, 21:54
 -- Wersja serwera: 10.4.25-MariaDB
 -- Wersja PHP: 8.1.10
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `block_login` (
   `id` int(11) NOT NULL,
   `badLoginNum` int(11) NOT NULL,
-  `lastBadLoginNum` int(11) DEFAULT NULL,
+  `lastBadLoginNum` varchar(11) DEFAULT NULL,
   `pernamentLock` tinyint(4) DEFAULT NULL,
   `tempLock` int(11) DEFAULT NULL,
   `ipAddress` varchar(11) NOT NULL
@@ -41,7 +41,7 @@ CREATE TABLE `block_login` (
 --
 
 INSERT INTO `block_login` (`id`, `badLoginNum`, `lastBadLoginNum`, `pernamentLock`, `tempLock`, `ipAddress`) VALUES
-(2, 0, NULL, NULL, NULL, '127.0.0.1');
+(3, 5, '2023-01-08 ', NULL, 1673211903, '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -63,20 +63,30 @@ CREATE TABLE `logged_in_users` (
 --
 
 INSERT INTO `logged_in_users` (`sessionId`, `userId`, `lastUpdate`, `logSuccess`, `id`, `ipAddress`) VALUES
-('', 138, '2023-01-08 19:40:09', 0, 77, '127.0.0.1'),
-('', 138, '2023-01-08 19:40:57', 0, 78, '127.0.0.1'),
-('', 138, '2023-01-08 19:41:13', 0, 79, '127.0.0.1'),
-('', 138, '2023-01-08 19:41:36', 0, 80, '127.0.0.1'),
-('', 138, '2023-01-08 19:41:48', 0, 81, '127.0.0.1'),
-('', 138, '2023-01-08 19:43:33', 0, 82, '127.0.0.1'),
-('fe0jhclc23e36gi9rtp98fr1ee', 138, '2023-01-08 19:43:59', 1, 83, '127.0.0.1'),
-('fe0jhclc23e36gi9rtp98fr1ee', 138, '2023-01-08 19:44:59', 1, 84, '127.0.0.1'),
-('fe0jhclc23e36gi9rtp98fr1ee', 138, '2023-01-08 19:45:02', 1, 85, '127.0.0.1'),
-('', 138, '2023-01-08 19:45:10', 0, 86, '127.0.0.1'),
-('', 138, '2023-01-08 19:45:27', 0, 87, '127.0.0.1'),
-('', 138, '2023-01-08 19:45:31', 0, 88, '127.0.0.1'),
 ('', 138, '2023-01-08 19:45:45', 0, 89, '127.0.0.1'),
-('fe0jhclc23e36gi9rtp98fr1ee', 138, '2023-01-08 19:57:05', 1, 90, '127.0.0.1');
+('fe0jhclc23e36gi9rtp98fr1ee', 138, '2023-01-08 19:57:05', 1, 90, '127.0.0.1'),
+('', 138, '2023-01-08 21:13:59', 0, 91, '127.0.0.1'),
+('', 138, '2023-01-08 21:14:14', 0, 92, '127.0.0.1'),
+('', 138, '2023-01-08 21:14:27', 0, 93, '127.0.0.1'),
+('cancdar4196cvhrph403s1e8ub', 138, '2023-01-08 21:30:20', 1, 94, '127.0.0.1'),
+('', 138, '2023-01-08 21:30:50', 0, 95, '127.0.0.1'),
+('', 138, '2023-01-08 21:32:25', 0, 96, '127.0.0.1'),
+('', 138, '2023-01-08 21:32:57', 0, 97, '127.0.0.1'),
+('', 138, '2023-01-08 21:33:17', 0, 98, '127.0.0.1'),
+('', 138, '2023-01-08 21:36:09', 0, 99, '127.0.0.1'),
+('', 138, '2023-01-08 21:36:53', 0, 100, '127.0.0.1'),
+('', 138, '2023-01-08 21:42:29', 0, 101, '127.0.0.1'),
+('', 138, '2023-01-08 21:43:00', 0, 102, '127.0.0.1'),
+('', 138, '2023-01-08 21:43:40', 0, 103, '127.0.0.1'),
+('cancdar4196cvhrph403s1e8ub', 139, '2023-01-08 21:44:40', 1, 104, '127.0.0.1'),
+('', 139, '2023-01-08 21:45:04', 0, 105, '127.0.0.1'),
+('v9qhslo0p22d8l5h479fe16phu', 139, '2023-01-08 21:47:55', 1, 106, '127.0.0.1'),
+('', 139, '2023-01-08 21:48:28', 0, 107, '127.0.0.1'),
+('', 139, '2023-01-08 21:49:32', 0, 108, '127.0.0.1'),
+('', 139, '2023-01-08 21:49:51', 0, 109, '127.0.0.1'),
+('', 139, '2023-01-08 21:52:18', 0, 110, '127.0.0.1'),
+('', 139, '2023-01-08 21:52:25', 0, 111, '127.0.0.1'),
+('', 139, '2023-01-08 21:52:33', 0, 112, '127.0.0.1');
 
 -- --------------------------------------------------------
 
@@ -123,7 +133,8 @@ INSERT INTO `users` (`id_user`, `login`, `email`, `password`, `salt`) VALUES
 (135, 'szymon', 'szsz@wp.pl', '$2y$10$3ngPNMusmxlxLVZvhU6W/eM3GVsi7yyo2TiQ8Pr/G7TSR4B.6XTVW', '2LcTZM396d'),
 (136, 'www', 'www@wp.pl', '$2y$10$NbP8TSLoSro0IqF2ahdfFedrY25v.va9Upoqxom9VFVZEQIDPkVwO', 'cCLpYM8Zg'),
 (137, 'szsz', 'wwaaw@wp.pl', '$2y$10$PhBgl2686d7rQWlIduKyuOa1kqvpU2Z4QxDlPbvrWzUKj/3IIZvke', 'sNIMjItIWc'),
-(138, 'ddd', 'ddd@wp.pl', '$2y$10$b0zsbXkB6g6.7wd2VUYtfO9MhQz7H5GUaFHgvYr.CXmZAsjzAB/cO', 'xX0Q22SyRN');
+(138, 'ddd', 'ddd@wp.pl', '$2y$10$b0zsbXkB6g6.7wd2VUYtfO9MhQz7H5GUaFHgvYr.CXmZAsjzAB/cO', 'xX0Q22SyRN'),
+(139, 'rrr', 'ddd@wp.pl', '$2y$10$/ehrVRDbtZa0PQ6GoD1hKOZbD6hYNAPkaUbkR2bwZ6MFp.mdW088e', '7UF2bVwu6');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -166,13 +177,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `block_login`
 --
 ALTER TABLE `block_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT dla tabeli `logged_in_users`
 --
 ALTER TABLE `logged_in_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT dla tabeli `password`
@@ -184,7 +195,7 @@ ALTER TABLE `password`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
 
 --
 -- Ograniczenia dla zrzutów tabel
